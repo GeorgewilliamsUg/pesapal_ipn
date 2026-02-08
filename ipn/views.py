156cf1d9a4a4727b03902e44851a2ad700350771
George <https://github.com/GeorgewilliamsUg/Nissi_medical_outreach.git>
2026-02-08 21:30:16 +0300

    setting the ipn

--- ipn/views.py
+++ ipn/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+import logging
 
-# Create your views here.
+
+logger = logging.getLogger(__name__)
+
+
+def pesapal_ipn(request):
+    data = request.GET.dict() or request.POST.dict()
+    logger.info(f"Received IPN data", extra={"data": data})
+
+  
+    return HttpResponse("IPN received", status=200)
+    
+
