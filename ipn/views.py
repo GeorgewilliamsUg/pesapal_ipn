1ee897c9c44286111705191043661e4fd0b7c44a
George <https://github.com/GeorgewilliamsUg/Nissi_medical_outreach.git>
2026-02-09 11:00:49 +0300

    setting more IPN

--- ipn/views.py
+++ ipn/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse
 import logging
-
+from django.views.decorators.csrf import csrf_exempt
 
 logger = logging.getLogger(__name__)
 
-
+@csrf_exempt
 def pesapal_ipn(request):
     data = request.GET.dict() or request.POST.dict()
-    logger.info(f"Received IPN data", extra={"data": data})
+    logger.info("Received IPN data", extra={"data": data})
 
-  
     return HttpResponse("IPN received", status=200)
-    
-
