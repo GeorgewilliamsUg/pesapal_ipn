b69ef19224262b0a80bda93a04574c7ad2ed80a2
George <https://github.com/GeorgewilliamsUg/Nissi_medical_outreach.git>
2026-02-11 10:15:58 +0300

    Correcting the Middleware order for CORS

--- pesapal_ipn/settings.py
+++ pesapal_ipn/settings.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
